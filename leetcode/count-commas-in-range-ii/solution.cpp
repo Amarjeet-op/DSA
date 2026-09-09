3c82df91ee5c481ffd82614689a564b66ee70325
AMARJEET ANAND <110993936+Amarjeet-op@users.noreply.github.com>
2026-09-09 10:42:04 +0530

    leetcode: save Count Commas in Range II solution

---
+++ leetcode/count-commas-in-range-ii/solution.cpp
@@ -0,0 +1,12 @@
+    long long countCommas(long long n) {
+public:
+        long long res=0,t=1000;
+        while(t<=n){
+            res+=(n-t+1) * 1ll;
+            t*=1000;
+        }
+class Solution {
+        return res;
+        
+    }
+};
